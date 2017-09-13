609b19e78ec2dfc49f7a7988b60bf8b835a31448
Chiang <chiangbinhao@gmail.com>
2017-09-13 19:22:48 +0800

    Bin second commit

--- Honk/app/src/main/java/MainActivity.java
+++ Honk/app/src/main/java/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
+        // test
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
