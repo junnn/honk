d651f4bccab07d3569e0ef3a329c9fbcbbfd60a5
Chiang <chiangbinhao@gmail.com>
2017-09-13 18:42:14 +0800

    Test commit
    Bin Commit Test Once Again...

--- Honk/app/src/main/java/MainActivity.java
+++ Honk/app/src/main/java/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Initialize DrawerLayout
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
