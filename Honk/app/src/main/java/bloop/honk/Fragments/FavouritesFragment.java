ea3a8133d76559ff3564a6b005c54302a8ad04eb
Bryan Boey S-15 <bryanboey@hotmail.com>
2017-09-25 11:25:51 +0800

    all

--- Honk/app/src/main/java/bloop/honk/Fragments/FavouritesFragment.java
+++ Honk/app/src/main/java/bloop/honk/Fragments/FavouritesFragment.java
@@ -1,10 +1,14 @@
 package bloop.honk.Fragments;
-
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import bloop.honk.R;
 
@@ -12,13 +16,35 @@ import bloop.honk.R;
  * Created by Jun Hao Ng on 6/9/2017.
  */
 
-public class FavouritesFragment extends Fragment {
+public class FavouritesFragment extends Fragment implements MyRecyclerViewAdapter.ItemClickListener {
+
+    MyRecyclerViewAdapter adapter;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View rootView = inflater.inflate(R.layout.fragment_favourites, container, false);
+
+        // data to populate the RecyclerView with
+        ArrayList<String> animalNames = new ArrayList<>();
+        animalNames.add("Horse");
+        animalNames.add("Cow");
+        animalNames.add("Camel");
+        animalNames.add("Sheep");
+        animalNames.add("Goat");
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.rvAnimals);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        adapter = new MyRecyclerViewAdapter(getActivity(),animalNames);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+        return rootView;
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_favourites, container, false);
-        getActivity().setTitle("Favourites");//set the title on the toolbar
-
-        return view;
+    public void onItemClick(View view, int position) {
+        Toast.makeText(getActivity(), "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
     }
 }
+
