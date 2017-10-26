21c573dc4d60f9905a154e43a86925ce8ad8c6bb
Bryan Boey S-15 <bryanboey@hotmail.com>
2017-10-26 17:07:31 +0800

    Changed my Bookmark fragment to use Bookmark controller instead. For now only got the GET function. After its solved then can add the others...

--- Honk/app/src/main/java/bloop/honk/Controller/BookmarkController.java
+++ Honk/app/src/main/java/bloop/honk/Controller/BookmarkController.java
@@ -1,15 +1,24 @@
 package bloop.honk.Controller;
 
 
+import android.app.Activity;
+import android.content.Context;
 import android.util.Log;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import bloop.honk.FavouritesComponents.Bookmark;
 
 /**
@@ -17,29 +26,45 @@ import bloop.honk.FavouritesComponents.Bookmark;
  */
 //TRIED MAKING but complete failure
 public class BookmarkController {
-    private List<Bookmark> posts;
-    private Gson gson;
-    private static final String readBk = "http://172.21.148.166/example/dao/Hookdaoimpl.php?function=getBookMark&username=admin";
+    private static BookmarkController instance = null;
+    private static Gson gson;
+    private static List<Bookmark> posts = new ArrayList<Bookmark>();
+    public static RequestQueue r;
+    private static boolean wait = true;
+    private static final String readBk = "http://172.21.148.166/example/dao/Hookdaoimpl.php?function=getBookMark&username=dummy";
 
-
-    public List<Bookmark> getBookmarkList(String username,RequestQueue r) {
-        Response.Listener<String> onPostsLoaded = new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                posts = Arrays.asList(gson.fromJson(response, Bookmark[].class));
-            }
-        };
-
-        Response.ErrorListener onPostsError = new Response.ErrorListener() {
+    public BookmarkController(String username,Activity activity) {
+        getBookmark(username,activity);
+    }
+    public static void getBookmark(String username, Activity activity) {
+        RequestQueue queue = Volley.newRequestQueue(activity);
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.setDateFormat("M/d/yy hh:mm a");
+        gson = gsonBuilder.create();
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, readBk,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        posts = Arrays.asList(gson.fromJson(response, Bookmark[].class));
+                        wait = false;
+                    }
+                }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Log.e("PostActivity", error.toString());
+
             }
-        };
-        StringRequest request = new StringRequest(Request.Method.GET, readBk, onPostsLoaded, onPostsError);
-        r.add(request);
-        while(posts == null);
-        return posts;
+        });
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 
+    public static boolean getstate() {
+        return wait;
+    }
+
+    public static List<Bookmark> returnList() {
+        return posts;
+    }
 }
