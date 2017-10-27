4385056936aeb4291b71b4fc9023c6c002a51d31
Don <noobie_donnie@hotmail.com>
2017-10-27 20:37:27 +0800

    Renamed to Manager. Added Controller.

--- Honk/app/src/main/java/bloop/honk/FeedsComponents/TrafficFeedController.java
+++ Honk/app/src/main/java/bloop/honk/FeedsComponents/TrafficFeedController.java
@@ -1,61 +1,22 @@
 package bloop.honk.FeedsComponents;
 
 import android.app.Activity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.util.Arrays;
 import java.util.List;
 
 /**
- * Created by Don on 25/10/2017.
+ * Created by Don on 27/10/2017.
  */
 
 public class TrafficFeedController {
-    private static final String NTUENDPOINT = "http://172.21.148.166/example/dao/Hookdaoimpl.php?function=getTrafficFeed";
-    private Activity activity;
-    private FeedsAdapter feedsAdapter;
-    private RequestQueue requestQueue;
-    private Gson gson;
+    private TrafficFeedManager man;
 
-
-    public TrafficFeedController(Activity activity, FeedsAdapter feedsAdapter){
-        this.activity = activity;
-        this.feedsAdapter = feedsAdapter;
+    public TrafficFeedController(Activity activity, FeedsAdapter feedsAdapter) {
+         man = new TrafficFeedManager(activity, feedsAdapter);
     }
 
-    public void fetchPostsNTU(final RecyclerView recyclerView, final List<FeedItem> feedList) {
-
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        gsonBuilder.setDateFormat("M/d/yy hh:mm a");
-        gson = gsonBuilder.create();
-        requestQueue = Volley.newRequestQueue(activity);
-
-        StringRequest request = new StringRequest(Request.Method.GET, NTUENDPOINT, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                feedList.clear();
-                feedList.addAll(Arrays.asList(gson.fromJson(response, FeedItem[].class)));
-                recyclerView.setAdapter(feedsAdapter);
-
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("PostActivity", error.toString());
-            }
-        });
-
-        requestQueue.add(request);
+    public void fetchPost(RecyclerView recyclerView, List<FeedItem> posts){
+         man.fetchPosts(recyclerView, posts);
     }
 }
--- Honk/app/src/main/java/bloop/honk/Fragments/FeedsFragment.java
+++ Honk/app/src/main/java/bloop/honk/Fragments/FeedsFragment.java
@@ -7,27 +7,13 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import bloop.honk.FeedsComponents.FeedItem;
 import bloop.honk.FeedsComponents.FeedsAdapter;
@@ -52,10 +38,11 @@ public class FeedsFragment extends Fragment {
         recyclerView = view.findViewById(R.id.feedrecycler);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        TrafficFeedController trafficFeedController = new TrafficFeedController(getActivity(), adapter);
+        TrafficFeedController TrafficFeedController = new TrafficFeedController(getActivity(), adapter);
 
         if (isNetworkConnected())
-            trafficFeedController.fetchPostsNTU(recyclerView, posts);
+            TrafficFeedController.fetchPost(recyclerView,posts);
+
         else
             Toast.makeText(getActivity(), "No Network", Toast.LENGTH_SHORT).show();
 
