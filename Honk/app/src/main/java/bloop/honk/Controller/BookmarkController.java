19293a8d94b4e5a79653097fc1ad742c3f0984bf
Bryan Boey S-15 <bryanboey@hotmail.com>
2017-10-28 14:55:52 +0800

    converted to entity + mgr

--- Honk/app/src/main/java/bloop/honk/Controller/BookmarkController.java
+++ Honk/app/src/main/java/bloop/honk/Controller/BookmarkController.java
@@ -26,6 +26,7 @@ import java.util.Map;
 
 import bloop.honk.FavouritesComponents.Bookmark;
 import bloop.honk.FavouritesComponents.BookmarkAdapter;
+import bloop.honk.FavouritesComponents.BookmarkManager;
 
 /**
  * Created by Bryan Boey S-15 on 22/10/2017.
@@ -36,20 +37,20 @@ public class BookmarkController {
 
     private BookmarkAdapter adapter;
     private Activity activity;
-    private Bookmark bookmark = new Bookmark();
+    private BookmarkManager bkMan = new BookmarkManager();
 
     public BookmarkController(Activity activity, BookmarkAdapter adapter) {
         this.activity = activity;
         this.adapter = adapter;
     }
     public void addBookmark(String username,String address, double lat, double lng ,final String url) {
-        bookmark.addBookmark(username,address,lat,lng,url,activity);
+        bkMan.addBookmark(username,address,lat,lng,url,activity);
     }
     public void getBookmarkList(String username, final RecyclerView recyclerView, final List<Bookmark> posts) {
-        bookmark.getBookmarkList(username, recyclerView, posts, activity, adapter);
+        bkMan.getBookmarkList(username, recyclerView, posts, activity, adapter);
     }
 
     public void deleteBookmark(final String username,final List<Bookmark> posts, final RecyclerView recyclerView, final String bkmk) {
-        bookmark.deleteBookmark(username,recyclerView,posts, bkmk, activity,adapter);
+        bkMan.deleteBookmark(username,recyclerView,posts, bkmk, activity,adapter);
     }
 }
--- Honk/app/src/main/java/bloop/honk/FavouritesComponents/Bookmark.java
+++ Honk/app/src/main/java/bloop/honk/FavouritesComponents/Bookmark.java
@@ -30,9 +30,6 @@ import java.util.Map;
 import bloop.honk.Config;
 
 public class Bookmark {
-    private static final String readBk = "http://172.21.148.166/example/dao/Hookdaoimpl.php?function=getBookMark&username=";
-    private static final String delBk = "http://172.21.148.166/example/dao/Hookdaoimpl.php?function=deletebookmark";
-    private static Gson gson;
     @SerializedName("favourite")
     private String name;
     private String longtitude;
@@ -59,107 +56,6 @@ public class Bookmark {
         return name;
     }
 
-    public void getBookmarkList(String username, final RecyclerView recyclerView, final List<Bookmark> posts, final Activity activity, final BookmarkAdapter adapter) {
-        RequestQueue queue = Volley.newRequestQueue(activity);
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        gsonBuilder.setDateFormat("M/d/yy hh:mm a");
-        gson = gsonBuilder.create();
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, readBk + username,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-
-                        if (response.equals("[]")) {
-                            Toast.makeText(activity.getApplicationContext(), "You currently have no bookmarks", Toast.LENGTH_SHORT).show();
-                        } else {
-                            if (posts.isEmpty()) {
-                                posts.clear();
-                            }
-                            posts.clear();
-                            posts.addAll(Arrays.asList(gson.fromJson(response, Bookmark[].class)));
-                            //adapter = new BookmarkAdapter(activity, posts);
-                            recyclerView.setAdapter(adapter);// data to populate the RecyclerView with
-                            //adapter.notifyDataSetChanged();
-
-                        }
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-            }
-        });
-// Add the request to the RequestQueue.
-        queue.add(stringRequest);
-    }
-
-    public void deleteBookmark(final String username, final RecyclerView recyclerView, final List<Bookmark> posts, final String bkmk, final Activity activity, final BookmarkAdapter adapter) {
-
-        RequestQueue requestQueue = Volley.newRequestQueue(activity);
-        StringRequest MyStringRequest = new StringRequest(Request.Method.POST, delBk, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                //fetchPosts();
-                getBookmarkList(username, recyclerView, posts, activity, adapter);
-                Toast.makeText(activity, "You unbookmarked FavButton " + bkmk, Toast.LENGTH_SHORT).show();
-                //This code is executed if the server responds, whether or not the response contains data.
-                //The String 'response' contains the server's response.
-            }
-        }, new Response.ErrorListener() { //Create an error listener to handle errors appropriately.
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                //This code is executed if there is an error.
-            }
-        }) {
-            protected Map<String, String> getParams() {
-                Map<String, String> MyData = new HashMap<String, String>();
-                MyData.put("username", username); //Add the data you'd like to send to the server.
-                MyData.put("bookmarkname", bkmk);
-                return MyData;
-            }
-        };
-        requestQueue.add(MyStringRequest);
-    }
-
-    public void addBookmark(final String username, final String address, final double lat, final double lng, final String url, final Activity activity) {
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                if (response.equalsIgnoreCase("unsucessful")) {
-                    Toast.makeText(activity, "Adding of Bookmark Failed", Toast.LENGTH_LONG).show();
-                } else if (response.equalsIgnoreCase("duplicate")) {
-                    Toast.makeText(activity, "This bookmark already exist in your list", Toast.LENGTH_LONG).show();
-                } else if (response.equalsIgnoreCase("success")) {
-                    Toast.makeText(activity, "You have added " + address + " into your Favourite List", Toast.LENGTH_SHORT).show();
-                    Log.i("android", "address: " + address + " lat: " + lat + " lng: " + lng);
-                } else {
-                    Toast.makeText(activity, "Unknown Error Occurred!", Toast.LENGTH_SHORT).show();
-                }
-
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-            }
-        }) {
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String, String> params = new HashMap<>();
-                //Adding parameters to request
-                params.put("username", username);
-                params.put("bookmarkname", address);
-                params.put("latitude", Double.toString(lat));
-                params.put("longtitude", Double.toString(lng));
-
-                return params;
-            }
-        };
-        RequestQueue requestQueue = Volley.newRequestQueue(activity);
-        requestQueue.add(stringRequest);
-    }
 }
 
 
